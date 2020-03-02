e7ce76c0afd6d3734c58a75c58932807c38fe82c
gabrielEloy <gabrieleloy@pop-os.localdomain>
2020-03-02 01:25:22 -0300

    added input basic styling

--- src/components/TextRangeInput/styles.js
+++ src/components/TextRangeInput/styles.js
@@ -4,6 +4,7 @@ export const Container = styled.div`
   display: flex;
   width: 100%;
   margin: 15px;
+  
   .label {
     margin-right: 10%;
   }
@@ -11,4 +12,58 @@ export const Container = styled.div`
   .range-input{
       width: 70%;
   }
+
+  input[type=range]{
+    -webkit-appearance: none;
+    background: transparent;
+  }
+  input[type=range]:focus {
+  outline: none;
+  }
+
+  input[type=range]::-ms-track {
+  width: 100%;
+  cursor: pointer;
+  background: transparent; 
+  border-color: transparent;
+  color: transparent;
+}
+
+input[type=range]::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  height: 16px;
+  width: 16px;
+  border-radius: 10px;
+  background: #ffffff;
+  cursor: pointer;
+  margin-top: -5px;
+}
+
+input[type=range]::-moz-range-thumb {
+  -webkit-appearance: none;
+  height: 16px;
+  width: 16px;
+  border: none;
+  border-radius: 10px;
+  background: #ffffff;
+  cursor: pointer;
+}
+
+input[type=range]::-webkit-slider-runnable-track {
+  width: 100%;
+  height: 6px;
+  cursor: pointer;
+  background: #3e3737;
+  border-radius: 2px;
+  border: 0.2px solid #010101;
+}
+
+input[type=range]::-moz-range-track {
+  width: 100%;
+  height: 6px;
+  cursor: pointer;
+  background: #3e3737;
+  border-radius: 5px;
+  border: 0.2px solid #010101;
+}
 `;
