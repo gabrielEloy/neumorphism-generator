27430fe7e371e885a431b6ddb44a0a00e317157a
gabrielEloy <gabrieleloy@pop-os.localdomain>
2020-03-08 23:18:58 -0300

    added copy button to copy box code content

--- src/components/CodeContainer/index.js
+++ src/components/CodeContainer/index.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from './styles';
 
 export default function CodeContainer({content}) {
+    const [copyText, setCopy] = useState('copy');
+    
     const keys = Object.keys(content)
-    console.log({content, keys})
+    function getContainerCode() {
+        let text = '';
+        for(let i in content){
+            text += `${i}: ${content[i]}; \n`;
+        }
+        navigator.clipboard.writeText(text).then(() => {
+            setCopy('copied');
+            setTimeout(() => setCopy('copy'), 1000);
+        });
+    }
     return (
         <Container>
+            <button onClick={getContainerCode} className="copy-button">
+                {copyText}
+            </button>
             {keys.map(key => (
                 <div className="property-container" key={key}>
                     <span className="property-name">
--- src/components/CodeContainer/styles.js
+++ src/components/CodeContainer/styles.js
@@ -3,13 +3,29 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   background: black;
-  /* height: 90px; */
   align-self: center;
   border-radius: 5px;
   display: flex;
   justify-content: center;
   align-items: flex-start;
   flex-direction: column;
+  position: relative;
+
+  .copy-button{
+    background: white;
+    position: absolute;
+    right:  3%;
+    top: 10%;
+    padding: 5px;
+    border: none;
+    border-radius: 3px;
+    :hover{
+      cursor: pointer;
+    }
+    :focus{
+      outline: none;
+    }
+  }
 
   .property-container {
     width: 70%;
