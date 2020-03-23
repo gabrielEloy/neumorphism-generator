83df428a01da4bdbd7d1535aa4180ddf5561ec76
gabrielEloy <gabrieleloy@pop-os.localdomain>
2020-03-23 02:04:28 -0300

    added color picker

--- src/components/ColorPickerSquare/styles.js
+++ src/components/ColorPickerSquare/styles.js
@@ -1,9 +1,19 @@
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 20px;
-  height: 20px;
-  background: ${props => props.color};
-  border: solid 2px black;
-`
+  input[type="color"] {
+    border: none;
+    width: 20px;
+    height: 20px;
+    -webkit-appearance: none;
+    ::-webkit-color-swatch-wrapper {
+      padding: 0;
+    }
+    ::-webkit-color-swatch {
+      border: solid black 2px;
+    }
+    :focus{
+      outline: none;
+    }
+  }
+`;
